27899b80098c6f6fd2e76076561db28d2f140942
Testspieler09 <142326461+Testspieler09@users.noreply.github.com>
2024-07-13 11:39:35 +0200

    implemented other input methods
    - radio button
    - just text input

--- src/Renderer.py
+++ src/Renderer.py
@@ -1,4 +1,5 @@
 from curses import newwin, newpad, initscr, init_pair, color_pair, start_color, curs_set, cbreak, noecho, nocbreak, echo, endwin, COLOR_BLACK, COLOR_BLUE, COLOR_GREEN, COLOR_RED, A_UNDERLINE, A_NORMAL
+from re import match
 from textwrap import wrap
 from os import get_terminal_size
 from assets import HELP_MESSAGE, FOOTER_TEXT
@@ -22,14 +23,16 @@ class PopUp:
         self.screen.nodelay(False)
 
     def kill_pop_up(self) -> None:
+        curs_set(0)
+        noecho()
         self.screen.nodelay(True)
-        del self.win
+        del self
 
     # Read input
     def get_input_checkboxes(self, options: list, message: str) -> list:
         message = self.make_message_fit_width(message, self.dimensions[1]-2)
         height_of_msg = len(message.splitlines())+1
-        self.win.addstr(1, 1, message)
+        self.win.addstr(1, 0, message)
         self.win.box()
         self.win.refresh()
 
@@ -54,6 +57,7 @@ class PopUp:
             pad.refresh(scroll_y, scroll_x, height_of_msg+3, 1, bottom_right_y, bottom_right_x)
 
         while True:
+            sleep(.01)
             display_menu()
             key = self.screen.getkey()
 
@@ -76,7 +80,80 @@ class PopUp:
         return [opt for idx, opt in enumerate(options) if selected[idx]]
 
     def get_input_radio_btn(self, options: list, message: str) -> str:
-        pass
+        message = self.make_message_fit_width(message, self.dimensions[1]-2)
+        height_of_msg = len(message.splitlines())+1
+        self.win.addstr(1, 0, message)
+        self.win.box()
+        self.win.refresh()
+
+        scroll_y, scroll_x = 0, 0
+        pad = newpad(len(options)+1, max(len(i) for i in options)+5)
+
+        current_option = 0
+
+        def display_menu():
+            for idx, option in enumerate(options):
+                if idx == current_option:
+                    pad.attron(color_pair(2))
+                    checkbox = "(X)"
+                else:
+                    checkbox = "( )"
+                pad.addstr(idx, 0, checkbox)
+                pad.addstr(idx, 4, option)
+                if idx == current_option: pad.attroff(color_pair(2))
+            bottom_right_y, bottom_right_x = height_of_msg+pad.getmaxyx()[0]+1, pad.getmaxyx()[1]+1
+            if height_of_msg+pad.getmaxyx()[0]+1 >= self.win.getmaxyx()[0]:
+                bottom_right_y = self.win.getmaxyx()[0]
+            if pad.getmaxyx()[1] >= self.win.getmaxyx()[1]-1:
+                bottom_right_x = self.win.getmaxyx()[1]-2
+            pad.refresh(scroll_y, scroll_x, height_of_msg+3, 1, bottom_right_y, bottom_right_x)
+
+        while True:
+            sleep(.01)
+            display_menu()
+            key = self.screen.getkey()
+
+            if key == "KEY_UP" and current_option > 0:
+                current_option -= 1
+                if current_option < scroll_y: scroll_y -= 1
+            elif key == "KEY_DOWN" and current_option < len(options) - 1:
+                current_option += 1
+                if current_option >= scroll_y + (self.win.getmaxyx()[0] - height_of_msg - 2): scroll_y += 1
+            elif key == "KEY_LEFT" and scroll_x > 0:
+                scroll_x -= 1
+            elif key == "KEY_RIGHT" and scroll_x < pad.getmaxyx()[1] - self.win.getmaxyx()[1] + 2:
+                scroll_x += 1
+            elif key == '\n':
+                break
+
+        self.kill_pop_up()
+        return options[current_option]
+
+    def get_input_string(self, p_message: str, input_length: int, p_regex=None) -> str:
+        WRONG_INPUT_MESSAGE = "The provided input doesn't match the wanted pattern.\n\n"
+        regex = r"[\S\s]*" if p_regex==None else p_regex
+        message = self.make_message_fit_width(p_message, self.dimensions[1]-2)
+        height_of_msg = len(message.splitlines())+1
+        message_is_updated = False
+        while True:
+            # Init new window and change some settings
+            self.win.erase()
+            echo()
+            curs_set(1)
+
+            # Output info and get input
+            self.win.addstr(1, 0, message)
+            self.win.box()
+            self.win.refresh()
+            input = self.win.getstr(height_of_msg, 1, input_length).decode('utf-8', 'backslashreplace')
+
+            if match(regex, input):
+                break
+            elif not message_is_updated:
+                message_is_updated = True
+                message = self.make_message_fit_width(WRONG_INPUT_MESSAGE + p_message, self.dimensions[1]-2)
+                height_of_msg = len(message.splitlines())+1
+        return input
 
     # Helper methods
     def make_message_fit_width(self, message: str, width: int) -> str:
@@ -204,9 +281,7 @@ class Renderer:
 
     # Implementations of the main procedures
     def search_procedure(self) -> None:
-        popup = PopUp(self.screen)
-        input = popup.get_input_checkboxes(["Python", "Java", "Go", "C", "C++"], "What programming languages do you use?")
-        print(input)
+        pass
 
     def add_procedure(self) -> None:
         pass
